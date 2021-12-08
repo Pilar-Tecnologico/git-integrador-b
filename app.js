eeb7ed56301e9c084677ce83281837388c43080d
fdaniel713 <fuentesdaniel342@gmail.com>
2021-12-08 15:04:54 -0300

    doc: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * return only movies with a goalscore of 80 or higher.
  * @returns {Array} of movie objects
  */
 function getOnlyGoodMovies(){
-    //complete with your code
+ /*
+   const good=  movies.filter(movie => {
+       if ( movie.metascore >= 80 ){
+           return true ; 
+           
+       } 
+       
+      
+   }) 
+   return good; 
+  */
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * return an array of links to the IMDb pages of each one
  * @returns {Array} of strings
  */
  function getGoodImdbLinks(){
-    //complete with your code
+
+   // const links = movies.map( link => link.imdb_title_id)
+    //console.log('https://www.imdb.com/title/'+links,'/\n');
+   
+   // return "https://www.imdb.com/title/"+links,"/\n";
+
+  
+
 }
+
 
 function main(){
     const goodMovies = getOnlyGoodMovies();
     const goodLinks = getGoodImdbLinks();
-    console.log(`Only movies with metascore of 80 or higher:\n${goodMovies}\n`);
+    //console.log('Only movies with metascore of 80 or higher:');
+   // console.log(goodMovies);
+   // console.log(goodLinks);
+    
+    console.log(`Only movies with metascore of 80 or higher:\n${(goodMovies)}\n`);
     console.log(`IMDb links of those good movies:\n${goodLinks}\n`);
 }
 
