54873c7053dcde976019aab0c5e7e2caec58f5ce
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2021-12-11 23:11:20 -0300

    Ejercicio 1 y Ejercicio 2

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const movies = require('./movies.json');
 
 /**
  * Returns movies with a metascore equal to 80 or higher. 
+ * For example: Uncut gems got a 91 metascore
+ * The response would be: "Uncut gems"
  * @returns {Array} of movie objects
  */
 function getOnlyGoodMovies(){
-    //complete with your code
+    
+    const metascore=80; 
+    return movies.find(movie => movie.metascore >= metascore);
+
 }
 
 /**
@@ -13,7 +18,10 @@ function getOnlyGoodMovies(){
  * @returns {Array} of strings
  */
  function getGoodImdbLinks(){
-    //complete with your code
+    const movie=getOnlyGoodMovies();
+
+    var salida= "https://www.imdb.com/title/,"+movie.imdb_title_id+"";
+    console.log(salida);
 }
 
 function main(){
